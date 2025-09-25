499b8f348b41388862d1d12f66b5b9ec4f19a04a
James <jdefowler@gmail.com>
2025-09-25 10:16:01 -0700

    bug fixes

--- src/pycno/functions.py
+++ src/pycno/functions.py
@@ -4,25 +4,34 @@ import roadrunner
 from pathlib import Path
 from tqdm.contrib.concurrent import process_map
 
-def set_parameter_values(sbml_model, parameter_list):
+def set_parameter_values(sbml_model, parameter_dict):
     for parameter in sbml_model.getListOfParameters():
-        for parameter_name in list(parameter_list.keys()):
+        for parameter_name in list(parameter_dict.keys()):
             if parameter.getName() == parameter_name:
-                parameter.setValue(float(parameter_list.pop(parameter_name)))
-    if parameter_list:
-        [print(f"Parameter {parameter_name} not found in the model.") for parameter_name in parameter_list.keys()]
+                parameter.setValue(float(parameter_dict.pop(parameter_name)))
+    if parameter_dict:
+        [print(f"Parameter {parameter_name} not found in the model.") for parameter_name in parameter_dict.keys()]
 
-def set_species_values(sbml_model, species_list):
-    for species_name in list(species_list.keys()):
+def set_species_values(sbml_model, species_dict):
+    for species_name in list(species_dict.keys()):
         for compartment in sbml_model.getListOfCompartments():
             if compartment.getName() == species_name.split('.')[0]:
                 break
         for species in sbml_model.getListOfSpecies():
             if species.getCompartment() == compartment.getId() and species.getName() == species_name.split('.')[1]:
-                species.setInitialAmount(species_list.pop(species_name))
+                species.setInitialAmount(species_dict.pop(species_name))
                 break
-    if species_list:
-        [print(f"Species {species_name} not found in the model.") for species_name in species_list.keys()]
+    if species_dict:
+        [print(f"Species {species_name} not found in the model.") for species_name in species_dict.keys()]
+
+def set_compartment_sizes(sbml_model, compartment_dict):
+    for comp in sbml_model.getListOfCompartments():
+        if comp.getName() in compartment_dict:
+            comp.setSize(float(compartment_dict[comp.getName()]))
+            compartment_dict.pop(comp.getName())
+    if compartment_dict:
+        for missing in compartment_dict.keys():
+            print(f"Compartment {missing} not found in the model.")
 
 def get_parameter(sbml_model, parameter_name):
     for parameter in sbml_model.getListOfParameters():
@@ -46,24 +55,23 @@ def get_species(species_name, result, rr, sbml_model):
         if species.getName() == species_name.split('.')[1]:
             return (result[f"[{species.getId()}]"] * compartment_size * NMOL2MBQ)
         
-def sum_region(region, species_name, result, sbml_model):
+def sum_region(region, species_name, result, sbml_model, rr):
     NMOL2MBQ = get_parameter(sbml_model, 'lambdaPhys') / 60 * 6.022e23 / 10**9 / 10**6
     total = np.zeros(result.shape[0])
     for compartment in sbml_model.getListOfCompartments():
             if region in compartment.getName():
                 compartment_id = compartment.getId()
-                compartment_size = compartment.getSize()
+                compartment_size = rr[f"{compartment_id}"]
                 for species in sbml_model.getListOfSpecies():
                     if species.getCompartment() == compartment_id and species_name in species.getName():
                         total += (result[f"[{species.getId()}]"] * compartment_size * NMOL2MBQ)
-
     return total
 
 def parameter_sweep(sbml_string, start, stop, steps, parameter_ids, swept_values):
     document = libsbml.readSBMLFromString(sbml_string)
     sbml_model = document.getModel()
     for i, id in enumerate(parameter_ids):
-        sbml_model.getParameter(id).setValue(swept_values[i])
+        sbml_model.getParameter(id).setValue(float(swept_values[i]))
 
     sbml_string = libsbml.writeSBMLToString(document)
     rr = roadrunner.RoadRunner(sbml_string)
@@ -80,6 +88,7 @@ def run_model(model_name: str = None,
             hotamount: float = 10, 
             coldamount: float = 100, 
             parameters: dict = None,
+            compartments: dict = None,
             observables: list = None,
             swept_parameters: list = None,
             swept_values: list = None
@@ -92,33 +101,11 @@ def run_model(model_name: str = None,
     document = reader.readSBML(model_path)
     sbml_model = document.getModel()
 
-    # Set parameters based on matlab variants (temporary fix)
-    varinat_values = {'Rden_Tumor1': 57,
-                      'Rden_Tumor2': 19,
-                      'Rden_SG': 38,
-                      'Rden_Kidney': 14,
-                      'lambdaRel_Tumor1': 1.5e-04,
-                      'lambdaRel_Tumor2': 1.5e-04,
-                      'lambdaRel_SG': 4.2e-04,
-                      'lambdaRel_Kidney': 2.9e-04,
-                      'TER_Kidney': 0.2,
-                      'bodySurfaceArea': 1.9,
-                      'bodyWeight': 100,
-                      'bodyHeight': 160,
-                      'f_SG': 0.0740,
-                      'R0_TumorRest': 13,
-                      
-                      'kPSAlb_Tumor1': 0,
-                      'kPSAlb_Tumor2': 0,
-                      'kPSAlb_TumorRest': 0,
-                      'k_on_toAlb': 0,
-                      'k_off_toAlb': 0,
-                      'AlbuminDen': 0,
-                      'K_D_Alb': 1}
-    set_parameter_values(sbml_model, varinat_values)
-
     if parameters:
         set_parameter_values(sbml_model, parameters)
+
+    if compartments:
+        set_compartment_sizes(sbml_model, compartments)
 
     if hotamount:
         if coldamount:
@@ -151,6 +138,6 @@ def run_model(model_name: str = None,
     TACs = np.zeros((num_curves, len(time), len(observables)))
     for curve in range(num_curves):
         for i, observable in enumerate(observables):
-            TACs[curve,:,i] = (sum_region(observable, 'Hot', result[curve], sbml_model))
+            TACs[curve,:,i] = (sum_region(observable, 'Hot', result[curve], sbml_model, rr))
 
     return time, TACs
