0a3556a8f4db912cbbae6c37d73174ae2155dc4a
James Fowler <jdefowler@gmail.com>
2025-10-01 10:20:23 -0700

    updated sbml conversion

--- matlab/SBMLConversion.m
+++ matlab/SBMLConversion.m
@@ -1,7 +1,7 @@
 %% Export model to SBML
-% simbiology('path_to_model.sbproj')
+% simbiology('AlbuminModelNewImplementation.sbproj')
 sbr=sbioroot;
 mc = sbr.Models(end);
 mn = copyobj(mc);
-commit(mc.variants, mn); % repeat for all variants if needed by indexing varinat number i. e. commit(mc.variants(1), mn)
+commit(mc.variants, mn); % specify variant(s) number if multiple variants exist i.e. commit(mc.variants(1), mn)
 sbmlexport(mn, 'output.sbml')
